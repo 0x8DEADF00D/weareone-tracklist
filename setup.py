4b0595a6b48ef02b2700c36b032e78e85f5cde22
Mostey <mostey1337@hotmail.de>
2014-12-10 13:52:52 +0100

    Implemented setup.py script and initialized parser package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+workingDirectory = os.path.abspath(os.path.dirname(__file__))
+readmeContent = open(os.path.join(workingDirectory, 'README.md')).read()
+
+setup(name='weareone-tracklist',
+      version='0.1',
+      description='Tracklist parser for WeAreOne radios written in Python',
+      long_description=readmeContent + '\n\n',
+      author='Mostey',
+      author_email='',
+      url='',
+      keywords='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['requests', 'beautifulsoup4'],
+      license="MIT",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.4'
+      ],
+      entry_points={
+          'console_scripts': [
+              'parser=parser:main',
+        ]
+      })
